ae7058d961f7042b43cb92374a7c368ee3efd9ee
jackraken <apple894894@gmail.com>
2015-03-09 16:53:09 +0800

    Add some comment to explain the code

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,19 +13,19 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, r6, r7, lr}
 
-	sub r4, r0, #0
-	mov r5, #0
-	mov r6, #1
-	mov r7, #1
+	sub r4, r0, #0	@ store r0(parameter) in r4
+	mov r5, #0      @ r5 is previous fib num 
+	mov r6, #1	@ r6 is current fib num
+	mov r7, #1	@ r7 is current index
 
-.L5:
-	cmp r4, r7
-	beq .L6
+.L5:			@ this is a loop
+	cmp r4, r7	@ if current index equals to the index we want  
+	beq .L6		@ break
 
-	add r7, #1
-	add r6, r5
-	sub r5, r6, r5
-	b .L5
+	add r7, #1	@ index += 1
+	add r6, r5	@ Fn = Fn-1 + Fn-2
+	sub r5, r6, r5	@ Fn-1 = Fn - Fn-2
+	b .L5		@ continue loop
 
 .L6:
 	sub r0, r6, #0
